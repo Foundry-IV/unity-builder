5bf0dd5987819c77c4ed6cc838f2839deed428b1
Max <max.leblanc@foundryiv.com>
2020-08-19 00:07:14 -0400

    add actual tag

--- src/model/docker.js
+++ src/model/docker.js
@@ -9,6 +9,7 @@ class Docker {
     let tag;
     let command;
     if (repository !== 'gableroux') {
+      tag = new ImageTag({ repository, name, version, platform });
       command = `docker build ${path} \
       --file ${dockerfile} \
       --build-arg IMAGE=${repository}/${name} \
